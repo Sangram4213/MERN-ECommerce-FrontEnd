44bbfb4ec7c0c128db783f08f36f68084dac1ec1
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-07-19 13:42:24 +0530

    create product cart

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,20 @@
+import {Link} from 'react-router-dom';
+import ProductCard from '../components/product-card';
 
 const Home = () => {
+  const addToCartHandler=()=>{
+
+  }
   return (
-    <div>Home</div>
+    <div className="home">
+      <section></section>
+      <h1>Latest Product
+        <Link to = '/search' className='findmore'>More</Link>
+      </h1>
+  <main>
+    <ProductCard productId='adf' name='Macbook' price={3211} stock={21} handler={addToCartHandler} photo='https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg'/>
+  </main>
+    </div>
   )
 }
 
