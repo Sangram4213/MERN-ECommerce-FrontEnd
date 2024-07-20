a8ce3276ea7ab350e3906c9ea0462a8bf8dd678a
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-07-20 17:25:32 +0530

    add UI of Search page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,29 @@
-import {Link} from 'react-router-dom';
-import ProductCard from '../components/product-card';
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 const Home = () => {
-  const addToCartHandler=()=>{
-
-  }
+  const addToCartHandler = () => {};
   return (
     <div className="home">
       <section></section>
-      <h1>Latest Product
-        <Link to = '/search' className='findmore'>More</Link>
+      <h1>
+        Latest Product
+        <Link to="/search" className="findmore">
+          More
+        </Link>
       </h1>
-  <main>
-    <ProductCard productId='adf' name='Macbook' price={3211} stock={21} handler={addToCartHandler} photo='https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg'/>
-  </main>
+      <main>
+        <ProductCard
+          productId="adf"
+          name="Macbook"
+          price={3211}
+          stock={21}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg"
+        />
+      </main>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
