d8e1b8abb8cc7f340af2c8984d3b77fd6b39eb22
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-09-07 16:20:30 +0530

    resolve cashfree type error

---
+++ src/types/cashfree-js.d.ts
@@ -0,0 +1,5 @@
+declare module '@cashfreepayments/cashfree-js' {
+    // Define the types based on the SDK's documentation or known usage
+    export function load(options?: { mode: string }): Promise<any>;
+  }
+  
