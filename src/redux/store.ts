c8f24fd4f206360de97414e96d89e869255acf1b
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-09-07 15:59:55 +0530

    remove mismatch is store middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,21 +11,21 @@ export const server = import.meta.env.VITE_SERVER;
 
 export const store = configureStore({
   reducer: {
-    // we take reducer name dynamic because in future if we change the name of reducer in slice here it is automatically change
     [userAPI.reducerPath]: userAPI.reducer,
-    [productAPI.reducerPath]:productAPI.reducer,
-    [dashboardApi.reducerPath]:dashboardApi.reducer,
-    [orderApi.reducerPath]:orderApi.reducer,
-    [userReducer.name] : userReducer.reducer,
-    [cartReducer.name] : cartReducer.reducer,
+    [productAPI.reducerPath]: productAPI.reducer,
+    [dashboardApi.reducerPath]: dashboardApi.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
+    [userReducer.name]: userReducer.reducer,
+    [cartReducer.name]: cartReducer.reducer,
   },
-  middleware: (mid) => [
-    ...mid(),
-    userAPI.middleware,
-    productAPI.middleware,
-    orderApi.middleware,
-    dashboardApi.middleware
-  ],
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      userAPI.middleware,
+      productAPI.middleware,
+      orderApi.middleware,
+      dashboardApi.middleware
+    ),
 });
+
 
 export type RootState = ReturnType<typeof store.getState>
